4c47d3799d245550ce75b168f20b35449b500806
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-13 11:20:33 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const colorInput = document.getElementById("seed-color");
+const schemeSelect = document.getElementById("scheme-mode");
+const getBtn = document.getElementById("get-scheme");
+const container = document.getElementById("color-container");
+
+getBtn.addEventListener("click", () => {
+  const seedHex = colorInput.value.substring(1);
+  const mode = schemeSelect.value;
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedHex}&mode=${mode}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      renderColors(data.colors);
+    });
+});
+
+function renderColors(colorsArray) {
+  let html = "";
+  colorsArray.forEach((color) => {
+    const hex = color.hex.value;
+    html += `
+            <div class="color-column">
+                <div class="color-swatch" style="background-color: ${hex}" data-hex="${hex}"></div>
+                <p class="hex-value" data-hex="${hex}">${hex}</p>
+            </div>
+        `;
+  });
+  container.innerHTML = html;
+}
